346bcbdcdec6a67fd251ee36b94a37fa5547bcd1
Yurii <yerikstar@gmail.com>
2025-08-14 22:03:58 +0300

    fix errors

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import "izitoast/dist/css/iziToast.min.css";
 
 import { getImagesByQuery } from "./js/pixabay-api";
 import {
- createGallery,
- clearGallery,
- showLoader,
- hideLoader,
- showLoadMoreButton,
- hideLoadMoreButton,
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+  showLoadMoreButton,
+  hideLoadMoreButton,
 } from "./js/render-functions";
 
 const form = document.querySelector(".form");
@@ -21,9 +21,54 @@ let totalHits = 0;
 form.addEventListener("submit", onFormSubmit);
 loadMoreBtn.addEventListener("click", onLoadMore);
 
+async function fetchImages(page) {
+  showLoader();
+  hideLoadMoreButton();
+
+  try {
+    const data = await getImagesByQuery(currentQuery, page);
+    const { hits, totalHits: hitsTotal } = data;
+
+    if (page === 1) {
+      totalHits = hitsTotal;
+      clearGallery();
+      if (hits.length === 0) {
+        iziToast.error({
+          title: "No results",
+          message: "Sorry, there are no images matching your search query.",
+        });
+        return;
+      }
+      iziToast.success({
+        title: "Success",
+        message: `We found ${totalHits} images.`,
+      });
+    }
+
+    createGallery(hits);
+
+    const galleryItemsCount = document.querySelectorAll(".gallery li").length;
+    if (galleryItemsCount < totalHits) {
+      showLoadMoreButton();
+    } else if (galleryItemsCount >= totalHits && page > 1) {
+      iziToast.info({
+        title: "End of results",
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+  } catch (err) {
+    iziToast.error({
+      title: "Error",
+      message: "Something went wrong. Please try again later.",
+    });
+    console.error(err);
+  } finally {
+    hideLoader();
+  }
+}
+
 async function onFormSubmit(e) {
   e.preventDefault();
-
   const query = e.target.elements["search-text"].value.trim();
   if (!query) {
     iziToast.error({
@@ -32,87 +77,16 @@ async function onFormSubmit(e) {
     });
     return;
   }
-
   currentQuery = query;
   currentPage = 1;
-  clearGallery();
-  hideLoadMoreButton();
-  showLoader();
-
-  try {
-    const data = await getImagesByQuery(currentQuery, currentPage);
-    const { hits, totalHits: hitsTotal } = data;
-    totalHits = hitsTotal;
-
-    if (hits.length === 0) {
-      iziToast.error({
-        title: "No results",
-        message: "Sorry, there are no images matching your search query. Please try again!",
-      });
-      return;
-    }
-
-    iziToast.success({
-      title: "Success",
-      message: `We found ${totalHits} images.`,
-    });
-
-    createGallery(hits);
-
-    const galleryItemsCount = document.querySelectorAll('.gallery li').length;
-    if (galleryItemsCount < totalHits) {
-      showLoadMoreButton();
-    } else {
-      iziToast.info({
-        title: "End of results",
-        message: "We're sorry, but you've reached the end of search results.",
-      });
-    }
-
-  } catch (err) {
-    iziToast.error({
-      title: "Error",
-      message: "Something went wrong. Please try again later.",
-    });
-    console.error(err);
-  } finally {
-    hideLoader();
-    form.reset();
-  }
+  await fetchImages(currentPage);
+  form.reset();
 }
 
 async function onLoadMore() {
   currentPage += 1;
-  showLoader();
-  hideLoadMoreButton();
-
-  try {
-    const data = await getImagesByQuery(currentQuery, currentPage);
-    const { hits } = data;
-
-    createGallery(hits);
-
-    const galleryItemsCount = document.querySelectorAll('.gallery li').length;
-    if (galleryItemsCount < totalHits) {
-      showLoadMoreButton();
-    } else {
-      iziToast.info({
-        title: "End of results",
-        message: "We're sorry, but you've reached the end of search results.",
-      });
-    }
-
-    smoothScroll();
-
-  } catch (err) {
-    iziToast.error({
-      title: "Error",
-      message: "Something went wrong. Please try again later.",
-    });
-    console.error(err);
-  } finally {
-    hideLoader();
-  }
+  await fetchImages(currentPage);
+  smoothScroll();
 }
 
 function smoothScroll() {
@@ -120,7 +94,6 @@ function smoothScroll() {
   if (!firstCard) return;
 
   const { height: cardHeight } = firstCard.getBoundingClientRect();
-
   window.scrollBy({
     top: cardHeight * 2,
     behavior: "smooth",
